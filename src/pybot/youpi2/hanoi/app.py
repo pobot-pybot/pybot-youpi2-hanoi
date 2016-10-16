877f6a613c7d67052bef9a839c2c7d044c358ba4
Eric Pascual <eric@pobot.org>
2016-10-16 15:37:53 +0200

    FSA refined (SOLVING split in two states)

--- src/pybot/youpi2/hanoi/app.py
+++ src/pybot/youpi2/hanoi/app.py
@@ -21,12 +21,19 @@ class HanoiDemoApp(YoupiApplication):
 
     DEFAULT_BASE_ANGLE = 25
 
-    STATE_INIT, STATE_READY, STATE_SOLVING, STATE_DONE = range(4)
+    STATE_INIT, STATE_READY, STATE_PICKING, STATE_PLACING, STATE_DONE = range(5)
     STATE_ABORT = -1
+    STATE_NAMES = ['ABORT', 'INIT', 'READY', 'PICKING', 'PLACING', 'DONE']
 
     TOWER_X = 130
     TOWER_Y_DIST = 110
     BLOCK_HEIGHT = 27
+
+    BLOCK_NAMES = ['A', 'B', 'C']   # A is the biggest one
+    BLOCK_SIZES = [69, 55, 39]  # mm
+    GRIPPER_STEPS_PER_MM = 92   # approx.
+    GRIPPER_CLOSED_STEPS = -10006
+    GRIPPER_RELEASE_MOVE = GRIPPER_STEPS_PER_MM * 10
 
     feed_pose = None
     kinematics = None
@@ -57,16 +64,19 @@ class HanoiDemoApp(YoupiApplication):
     ]
 
     # number of blocks in each position
-    tower_heights = [3, 0, 0]       # we start from the left side => leftmost one is full
+    towers = [[0, 1, 2], [], []]      # we start from the left side => leftmost one is full
 
     step_num = 0
-    gripper_empty = True
+    # gripper_empty = True
+    moved_block = None
     from_pose = to_pose = None
 
     start_x, start_y = TOWER_X, 0
     current_x = current_y = None
 
     ok_esc_line = None
+
+    blk_closed_pos = [None] * 3
 
     state = STATE_INIT
 
@@ -82,14 +92,14 @@ class HanoiDemoApp(YoupiApplication):
         not to collide with towers. """
         if abs(to_tower - from_tower) == 1:
             # from and to positions are contiguous => it is the highest of both
-            level = max(self.tower_heights[to_tower], self.tower_heights[from_tower])
+            level = max(len(self.towers[to_tower]), len(self.towers[from_tower]))
         else:
             # if we have to "skip over" the 3rd tower, the travel level is the one
-            # above every body
-            level = max(self.tower_heights)
+            # above everybody
+            level = max([len(t) for t in self.towers])
 
         # take into account the extra height of the carried block, if any
-        return level if self.gripper_empty else (level + 0.5)
+        return level if self.moved_block is None else level + 0.5
 
     def setup(self, **kwargs):
         self.arm.soft_hi_Z()
@@ -105,11 +115,24 @@ class HanoiDemoApp(YoupiApplication):
         self.pnl.write_at(self.ok_esc_line, line=1)
         return self.pnl.wait_for_key(valid=[Keys.OK, Keys.ESC], blink=True, max_wait=max_wait) == Keys.OK
 
+    def _state_name(self, state):
+        return self.STATE_NAMES[state + 1]
+
+    # TODO make this work
+    # def _open_gripper_for_block(self, blk_num):
+    #     width = self.BLOCK_SIZES[blk_num] + 5
+    #     steps = self.GRIPPER_CLOSED_STEPS + width * self.GRIPPER_STEPS_PER_MM
+    #     self.arm.motor_goto({YoupiArm.MOTOR_GRIPPER: steps})
+    #
+    # def _release_block(self):
+    #     self.arm.motor_move({YoupiArm.MOTOR_GRIPPER: self.GRIPPER_RELEASE_MOVE})
+
     def make_ready(self):
         self.pnl.clear()
         self.pnl.center_text_at('Set arm hands up', line=2)
         self.pnl.center_text_at('ESC:quit - OK:done', line=4)
 
+        self.log_info('waiting for the arm be put in ready position...')
         if not self._ok_cancel():
             return self.STATE_ABORT
 
@@ -117,12 +140,15 @@ class HanoiDemoApp(YoupiApplication):
         self.pnl.center_text_at('Already calibrated ?', line=2)
         self.pnl.center_text_at('ESC:no - OK:yes', line=4)
 
+        self.log_info('is arm already calibrated ?')
         if self._ok_cancel():
+            self.log_info('--> already calibrated')
             self.pnl.center_text_at('Moving home.', line=2)
             self.pnl.center_text_at('Please wait...', line=4)
             self.arm.go_home()
             self.arm.open_gripper()
         else:
+            self.log_info('--> calibrating...')
             self.pnl.center_text_at('Calibrating.', line=2)
             self.pnl.center_text_at('Please wait...', line=4)
             self.arm.seek_origins()
@@ -130,24 +156,33 @@ class HanoiDemoApp(YoupiApplication):
 
         self.pnl.clear()
         self.pnl.center_text_at('Initial setup...', line=2)
+        self.log_info('preparing tower...')
 
         feed_pose_hover = self._compute_pose(self.TOWER_X, 0, level=0.5)
         delta_shoulder = feed_pose_hover[YoupiArm.MOTOR_SHOULDER] - self.feed_pose[YoupiArm.MOTOR_SHOULDER]
 
-        blk_nums = ['1st', '2nd', '3rd']
+        gripper_pos = self.arm.get_motor_positions()[YoupiArm.MOTOR_GRIPPER]
+        self.log_info("gripper opened position : %d", gripper_pos)
+
         for i in range(3):
+            blk_name = self.BLOCK_NAMES[i]
+
             self.arm.goto(self.feed_pose)
 
             self.pnl.clear()
-            self.pnl.center_text_at('Give me %s block' % blk_nums[i], line=2)
+            self.pnl.center_text_at('Give me block %s' % blk_name, line=2)
             self.pnl.center_text_at('ESC:quit - OK:done', line=4)
+            self.log_info('waiting for block %s...', blk_name)
             if not self._ok_cancel(max_wait=120):
                 return self.STATE_ABORT
 
             self.pnl.clear()
 
+            self.log_info('.. picking...')
             self.pnl.center_text_at('Centering block...', line=2)
             self.arm.close_gripper()
+            self.blk_closed_pos[i] = gripper_pos = self.arm.get_motor_positions()[YoupiArm.MOTOR_GRIPPER]
+            self.log_info("gripper closed position for block %s : %d", self.BLOCK_NAMES[i], gripper_pos)
             self.arm.open_gripper()
             self.arm.motor_move({YoupiArm.MOTOR_SHOULDER: delta_shoulder})
             self.arm.rotate_hand_to(90)
@@ -155,6 +190,7 @@ class HanoiDemoApp(YoupiApplication):
             self.arm.close_gripper()
 
             self.pnl.center_text_at('Installing block...', line=2)
+            self.log_info('.. installing...')
 
             self.arm.goto(self._compute_pose(self.TOWER_X, 0, level=i + 0.5, hand_rot=90))
 
@@ -190,94 +226,127 @@ class HanoiDemoApp(YoupiApplication):
         self.pnl.center_text_at('Ready.', line=2)
         self.pnl.center_text_at('ESC:quit - OK:go', line=4)
 
+        self.log_info('ready - asking for what to do now...')
         if not self._ok_cancel(max_wait=120):
+            self.log_info('.. abort requested')
             return self.STATE_ABORT
 
         return self.STATE_READY
 
     def start_solving(self):
-        self.clear_screen()
-        self.pnl.center_text_at('Solving puzzle...', line=3)
+        self.pnl.clear()
+        self.pnl.center_text_at('Solving puzzle...', line=2)
+        self.pnl.center_text_at('RED key to abort', line=4)
+        self.pnl.set_leds([Keys.ESC])
 
         self.current_x, self.current_y = self.start_x, self.start_y
 
-        return self.STATE_SOLVING
+        return self.STATE_PICKING
 
-    def move_things(self):
+    def _arm_action(self, meth, *args, **kwargs):
+        """ Wraps an arm action request with the checking of the abort key use. """
+        meth(*args, **kwargs)
+        if Keys.ESC in self.pnl.get_keys():
+            raise KeyboardInterrupt()
+
+    def pick_block(self):
         from_side, to_side = (n * self.direction for n in self.sequence[self.step_num])
         from_tower, to_tower = from_side + 1, to_side + 1
 
-        # use a more natural tower numbering (i.e. starting from 1)
-        self.pnl.center_text_at("From %d to %d..." % (from_tower + 1, to_tower + 1), line=3)
-        still_to_do = len(self.sequence) - self.step_num
-        if still_to_do > 1:
-            msg = "Still %d moves to do" % still_to_do
-        else:
-            msg = "Last move !!"
-        self.pnl.center_text_at(msg, line=4)
-
-        # raise enough depending on the current tower heights to fly to the target tower
+        # compute the travel level depending on the current tower contents to fly to the target tower
         # without colliding
         travel_level = self.compute_travel_level(from_tower, to_tower)
-        self.arm.goto(self._compute_pose(self.current_x, self.current_y, travel_level))
 
-        if self.gripper_empty:
-            # first half of the move : we go and pick the block to move
+        try:
+            # climb at the right altitude before moving
+            self._arm_action(self.arm.goto, self._compute_pose(self.current_x, self.current_y, travel_level))
 
-            from_level = self.tower_heights[from_tower] - 1     # levels are numbered from 0
+            next_block = self.towers[from_tower][-1]
+            blk_name = self.BLOCK_NAMES[next_block]
+
+            # use a more natural tower numbering (i.e. starting from 1)
+            human_friendly_from, human_friendly_to = from_tower + 1, to_tower + 1
+
+            msg = "%s from %d to %d..." % (blk_name, human_friendly_from, human_friendly_to)
+            self.pnl.center_text_at(msg, line=1)
+            self.log_info('picking block %s from tower %d...', blk_name, human_friendly_from)
+
+            still_to_do = len(self.sequence) - self.step_num
+            if still_to_do > 1:
+                msg = "Still %d moves to do" % still_to_do
+            else:
+                msg = "Last move !!"
+            self.pnl.center_text_at(msg, line=2)
+
+            from_level = len(self.towers[from_tower]) - 1  # levels are numbered from 0
             new_x, new_y = self.TOWER_X, self.TOWER_Y_DIST * from_side
 
             # move over the pick position
-            self.arm.goto(self._compute_pose(new_x, new_y, travel_level))
-            # go down to the position
-            self.arm.goto(self._compute_pose(new_x, new_y, from_level))
-            # pick the block
-            self.arm.close_gripper()
+            self._arm_action(self.arm.goto, self._compute_pose(new_x, new_y, travel_level))
+            # go down to the block
+            self._arm_action(self.arm.goto, self._compute_pose(new_x, new_y, from_level))
+            # pick it
+            self._arm_action(self.arm.close_gripper)
+            self.moved_block = self.towers[from_tower].pop()
 
             self.current_x, self.current_y = new_x, new_y
+            return self.STATE_PLACING
 
-            # since we pick a block, the tower is now one unit smaller
-            self.tower_heights[from_tower] -= 1
-            # and we are carrying something
-            self.gripper_empty = False
+        except KeyboardInterrupt:
+            self.log_warning('ESC key pressed => abort')
+            return self.STATE_ABORT
 
-        else:
+    def place_block(self):
+        from_side, to_side = (n * self.direction for n in self.sequence[self.step_num])
+        from_tower, to_tower = from_side + 1, to_side + 1
+        human_friendly_to = to_tower + 1
+        blk_name = self.BLOCK_NAMES[self.moved_block]
+
+        # compute the travel level depending on the current tower contents to fly to the target tower
+        # without colliding
+        travel_level = self.compute_travel_level(from_tower, to_tower)
+
+        try:
+            # climb at the right altitude before moving
+            self._arm_action(self.arm.goto, self._compute_pose(self.current_x, self.current_y, travel_level))
+
             # second half of the move : we bring the block to its destination tower
+            self.log_info('placing block %s on tower %d...', blk_name, human_friendly_to)
 
-            to_level = self.tower_heights[to_tower]     # levels are numbered from 0
+            to_level = len(self.towers[to_tower])  # levels are numbered from 0
             new_x, new_y = self.TOWER_X, self.TOWER_Y_DIST * to_side
 
             # move over the drop position
-            self.arm.goto(self._compute_pose(new_x, new_y, travel_level))
-            # go down to the position
-            self.arm.goto(self._compute_pose(new_x, new_y, to_level))
+            self._arm_action(self.arm.goto, self._compute_pose(new_x, new_y, travel_level))
+            # go down to it
+            self._arm_action(self.arm.goto, self._compute_pose(new_x, new_y, to_level))
             # release the block
-            self.arm.open_gripper()
+            self._arm_action(self.arm.open_gripper)
 
             self.current_x, self.current_y = new_x, new_y
 
-            # the destination tower is not une unit taller
-            self.tower_heights[to_tower] += 1
-
-            # we are carrying something no more
-            self.gripper_empty = True
+            # drop the block
+            self.towers[to_tower].append(self.moved_block)
+            self.moved_block = None
 
             self.step_num += 1
-            if self.step_num == len(self.sequence):
-                return self.STATE_DONE
+            return self.STATE_DONE if self.step_num == len(self.sequence) else self.STATE_PICKING
 
-        # we are not finished => stay in the current state
-        return self.state
+        except KeyboardInterrupt:
+            self.log_warning('ESC key pressed => abort')
+            return self.STATE_ABORT
 
     def again_or_enough(self):
         self.pnl.clear()
         self.pnl.center_text_at('Job done !!', line=2)
+        self.log_info('Done')
 
         # final motion to the ready pose
         self.arm.goto(self._compute_pose(self.current_x, self.current_y, self.compute_travel_level(-1, +1)))
         self.arm.goto(self.ready_pose)
 
         self.pnl.center_text_at('ESC:quit - OK:again', line=4)
+        self.log_info('asking for what to do now...')
         if self._ok_cancel(max_wait=120):
             self.step_num = 0
             self.direction = -self.direction
@@ -287,6 +356,8 @@ class HanoiDemoApp(YoupiApplication):
             return self.STATE_ABORT
 
     def loop(self):
+        self.log_info('current state: %s', self._state_name(self.state))
+
         if self.state == self.STATE_INIT:
             self.state = self.make_ready()
             return self.state == self.STATE_ABORT
@@ -294,12 +365,25 @@ class HanoiDemoApp(YoupiApplication):
         elif self.state == self.STATE_READY:
             self.state = self.start_solving()
 
-        elif self.state == self.STATE_SOLVING:
-            self.state = self.move_things()
+        elif self.state == self.STATE_PICKING:
+            self.state = self.pick_block()
+
+            if self.state == self.STATE_ABORT:
+                self.pnl.clear()
+                self.pnl.center_text_at('Aborted !!', line=2)
+
+        elif self.state == self.STATE_PLACING:
+            self.state = self.place_block()
+
+            if self.state == self.STATE_ABORT:
+                self.pnl.clear()
+                self.pnl.center_text_at('Aborted !!', line=2)
 
         elif self.state == self.STATE_DONE:
             self.state = self.again_or_enough()
-            return self.state == self.STATE_ABORT
+
+        elif self.state == self.STATE_ABORT:
+            return True
 
         else:
             raise RuntimeError('invalid state (%s)' % self.state)
